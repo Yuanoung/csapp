1bf54a8aad7c8e07f3400cf7c364306c47dc107f
yuanoung <Yuanoung@users.noreply.github.com>
2018-08-23 15:33:00 +0800

    Update ex2-68.c

--- exercise/ex2-68.c
+++ exercise/ex2-68.c
@@ -7,19 +7,20 @@
  * Example: n = 6 --> 0x3f, n = 17 --> 0x1FFFF
  * Assume 1 <= n <= w
  *
- *         int lower_bits(int x, int n);
+ *         int lower_one_mask(int x, int n);
  *
  * 函数应该遵循位级整数编码规则。要注意 n = w 的情况
  */
 
-int lower_bits(int x, int n)
-{
-        int r = 0;
-        int s = n - 1;
+#include <stdio.h>
 
-        /* 1. 构造一个最低 n 位为 1 的数 */
-        r = (1<<s) - 1 + (1<<s);
+int lower_one_mask(int n) {
+	return (1<< (n-1))-1 | (1<<(n-1));
+}
 
-        /* 2. 将这个数与原来的数字进行或运算作为结果返回 */
-        return x | r;
+int main() {
+	printf("%d\n", lower_one_mask(6));
+	printf("%d\n", lower_one_mask(17));
+	printf("%d\n", lower_one_mask(1));
+	printf("%d\n", lower_one_mask(32));
 }
