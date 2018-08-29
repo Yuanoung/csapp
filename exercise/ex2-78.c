540a59282b1cb11e2910574836e511e73074e38e
yuanoung <Yuanoung@users.noreply.github.com>
2018-08-29 11:27:22 +0800

    Update ex2-78.c

--- exercise/ex2-78.c
+++ exercise/ex2-78.c
@@ -9,7 +9,7 @@
 
 int mul5div8(int x)
 {
-        int sign = (x & INT_MIN) == INT_MIN;
+        int sign = !((x & INT_MIN) ^ INT_MIN);
         int bias = (7 + !sign) & 7;
         int t = (x << 2) + x;
         return (t + bias) >> 3;
